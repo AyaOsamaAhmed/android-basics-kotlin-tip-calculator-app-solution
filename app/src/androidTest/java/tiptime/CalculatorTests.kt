1ee4e781bde2e48ffde047ffad9db31d982d0786
Aya Osama Ahmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2022-04-22 13:18:57 +0200

    finish testing view

--- app/src/androidTest/java/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/tiptime/CalculatorTests.kt
@@ -1,12 +1,17 @@
 package tiptime
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.tiptime.MainActivity
 import com.example.tiptime.R
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,6 +26,13 @@ class CalculatorTests {
     fun calculate_20_percent_tip() {
         onView(withId(R.id.cost_of_service_edit_text))
             .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$10.00"))))
     }
 
 }
